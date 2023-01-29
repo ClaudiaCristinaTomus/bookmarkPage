6ab9488850ad6fd65455c6edd13dc662de229f07
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-01-29 15:24:37 +0200

    4th commit

--- script.js
+++ script.js
@@ -4,22 +4,48 @@ const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
 
 
+const faqs = document.querySelectorAll(".faq1");
 
+const tabs = document.querySelectorAll('.tab_btn');
+const all_content = document.querySelectorAll('.content');
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
 
 // close menu when you click on a menu item 
 menu_items.forEach(item => {
-    item.addEventListener('click',function(){
+    item.addEventListener('click', function () {
         close();
     })
 })
 
-function show(){
+function show() {
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
 }
-function close(){
+function close() {
     mainMenu.style.top = '-100%';
 }
+
+
+faqs.forEach((faq1) => {
+    faq1.addEventListener("click", () => {
+        faq1.classList.toggle("active");
+    });
+});
+
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', (e) => {
+        tabs.forEach(tab => { tab.classList.remove('active') });
+        tab.classList.add('active');
+        const line = document.querySelector('.line');
+        line.style.width = e.target.offsetWidth + "px";
+        line.style.left = e.target.offsetLeft + "px";
+
+        all_content.forEach(content => { content.classList.remove('active') });
+
+        all_content[index].classList.add('active');
+    })
+
+})
